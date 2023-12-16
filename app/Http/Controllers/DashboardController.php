ff74b6782301e7904e2f87f6473292c933778697
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-16 00:19:00 -0300

    feat: adiciona funcao para buscar informacoes do dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,7 +12,21 @@ class DashboardController extends Controller
     public function index()
     {
         try {
-            // lógica aqui
+            $user = auth()->user();
+
+            $amountStudents = Student::where('user_id', $user->id)->count();
+            $amountExercises = Exercise::where('user_id', $user->id)->count();
+            $currentPlan = $user->plan->description;
+            $planLimit = $user->plan->limit;
+            $remainingPlan = $planLimit ? $planLimit - $amountStudents : null;
+
+            return $this->response('', Response::HTTP_OK, [
+                'registered_students' => $amountStudents,
+                'registered_exercises' => $amountExercises,
+                'current_user_plan' => "Plano $currentPlan",
+                'remaining_students' => $remainingPlan
+
+            ]);
         } catch (\Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
