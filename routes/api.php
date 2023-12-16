73c1d5fe03e58ff9e4969a075252223cb17fc48d
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-16 16:30:16 -0300

    feat: adiciona rota para cadastro de exercicio

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index']);
+
+    Route::post('exercises', [ExerciseController::class, 'store']);
 });
 
 Route::post('users', [UserController::class, 'store']);
