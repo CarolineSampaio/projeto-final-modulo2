3a9ecd45c5a11a7aa8233692db3205f70239a6b9
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-16 17:17:25 -0300

    feat: adiciona funcao para cadastro de exercicio

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExerciseController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
+        try {
+            $data =  $request->only(['description']);
+
+            $request->validate([
+                'description' => 'string|required|max:255',
+            ]);
+
+            $data['description'] = strtolower($data['description']);
+            $data['user_id'] = auth()->user()->id;
+
+            $exerciseExists = Exercise::where('description', $data['description'])
+                ->where('user_id', $data['user_id'])
+                ->first();
+
+            if ($exerciseExists) {
+                return $this->error('Conflito ao realizar cadastro. Este exercício já existe em sua lista.', Response::HTTP_CONFLICT);
+            }
+
+            $exercise = Exercise::create($data);
+
+            return $this->response('Exercício cadastrado com sucesso.', Response::HTTP_CREATED, $exercise->makeHidden(['user_id']));
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
