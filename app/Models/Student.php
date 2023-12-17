ec03931c195b3af09a106a366c8e59a07a256b00
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-16 23:23:42 -0300

    feat: adiciona migration para criacao da tabela students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'date_birth',
+        'cpf',
+        'contact',
+        'user_id',
+        'city',
+        'neighborhood',
+        'number',
+        'street',
+        'state',
+        'cep',
+    ];
 }
