067f149c7489c68bd6c708e3aa377e1d5e1230f9
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-24 17:44:20 -0300

    feat: adiciona factory para Student

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'date_birth' => $this->faker->date,
+            'cpf' => $this->faker->numerify('###########'),
+            'contact' => $this->faker->phoneNumber,
+            'cep' => $this->faker->numerify('########'),
+            'street' => $this->faker->streetName,
+            'state' => $this->faker->stateAbbr,
+            'neighborhood' => $this->faker->city,
+            'city' => $this->faker->city,
+            'number' => $this->faker->randomNumber(3),
+        ];
+    }
+}
