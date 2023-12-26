0537c8ea0a0b2d2eac4108cb7ad0cf87c122f386
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-26 20:55:29 -0300

    feat: adiciona refresh database a classe test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,12 @@
 namespace Tests;
 
 use Database\Seeders\PopulatePlans;
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
     use CreatesApplication;
     protected $seeder = PopulatePlans::class;
 }
