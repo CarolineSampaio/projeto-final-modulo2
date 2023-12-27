017afa8e15245d932c8988ff1da3bf59ae802d82
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-27 20:38:32 -0300

    chore: simplifica metodo store exercise controller

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -10,30 +10,22 @@ class ExerciseController extends Controller
 {
     public function store(Request $request)
     {
-        try {
-            $data =  $request->only(['description']);
+        $data =  $request->only(['description']);
+        $request->validate([
+            'description' => 'string|required|max:255',
+        ]);
 
-            $request->validate([
-                'description' => 'string|required|max:255',
-            ]);
+        $data['description'] = strtolower($data['description']);
+        $data['user_id'] = auth()->user()->id;
 
-            $data['description'] = strtolower($data['description']);
-            $data['user_id'] = auth()->user()->id;
+        $exerciseExists = Exercise::where('description', $data['description'])
+            ->where('user_id', $data['user_id'])
+            ->first();
 
-            $exerciseExists = Exercise::where('description', $data['description'])
-                ->where('user_id', $data['user_id'])
-                ->first();
+        if ($exerciseExists) return $this->error('Conflito ao realizar cadastro. Este exercício já existe em sua lista.', Response::HTTP_CONFLICT);
 
-            if ($exerciseExists) {
-                return $this->error('Conflito ao realizar cadastro. Este exercício já existe em sua lista.', Response::HTTP_CONFLICT);
-            }
-
-            $exercise = Exercise::create($data);
-
-            return $this->response('Exercício cadastrado com sucesso.', Response::HTTP_CREATED, $exercise->makeHidden(['user_id']));
-        } catch (\Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
-        }
+        $exercise = Exercise::create($data);
+        return $this->response('Exercício cadastrado com sucesso.', Response::HTTP_CREATED, $exercise->makeHidden(['user_id']));
     }
 
     public function index()
